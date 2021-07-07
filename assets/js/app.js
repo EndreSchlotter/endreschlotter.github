02a011e000b93862a7ae0de37b478d6a5eab34f4
Slotyi <hjomiland@gmail.com>
2021-07-08 00:41:52 +0200

    changed height calculations

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,29 +9,29 @@ const linksContainer = document.querySelector('.links-container');
 const links = document.querySelector('.links');
 
 navToggle.addEventListener('click', () => {
-   const containerHeight = linksContainer.getBoundingClientRect().height;
-   const linksHeight = links.getBoundingClientRect().height;
-   if(containerHeight === 0) {
-       linksContainer.style.height = `${linksHeight}px`
-   } else {
-       linksContainer.style.height = 0;
-   }
+    const containerHeight = linksContainer.offsetHeight;
+    const linksHeight = links.offsetHeight;
+    if (containerHeight === 0) {
+        linksContainer.style.height = `${linksHeight}px`
+    } else {
+        linksContainer.style.height = 0;
+    }
 });
 
 /*************  fixed navbar and back to top button ****************/
 const navbar = document.getElementById('nav');
 const topLink = document.querySelector('.top-link');
 
-window.addEventListener('scroll', ()=> {
+window.addEventListener('scroll', () => {
     const scrollHeight = window.pageYOffset;
-    const navHeight = navbar.getBoundingClientRect().height;
-    if(scrollHeight > navHeight) {
+    const navHeight = navbar.offsetHeight;
+    if (scrollHeight > navHeight) {
         navbar.classList.add('fixed-nav');
     } else {
         navbar.classList.remove('fixed-nav');
     }
 
-    if(scrollHeight > 500) {
+    if (scrollHeight > 500) {
         topLink.classList.add('show-link');
     } else {
         topLink.classList.remove('show-link');
@@ -52,16 +52,16 @@ scrollLinks.forEach(link => {
         const id = actualHref.substr(indexOfHashtag + 1);
         const element = document.getElementById(id);
         // calculate the heights
-        const navHeight = navbar.getBoundingClientRect().height;
-        const containerHeight = linksContainer.getBoundingClientRect().height;
+        const navHeight = navbar.offsetHeight;
+        const containerHeight = linksContainer.offsetHeight;
         const fixedNav = navbar.classList.contains('fixed-nav');
         let position = element.offsetTop - navHeight;
 
-        if(!fixedNav) {
+        if (!fixedNav) {
             position = position - navHeight;
         }
 
-        if(navHeight > 74) {
+        if (navHeight > 74) {
             position = position + containerHeight;
         }
 
